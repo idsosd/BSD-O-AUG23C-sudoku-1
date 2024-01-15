8ff5be6cef18fd6564706a1341b83d8f2f2271ae
ip.osinga <ip.osinga@alfa-college.nl>
2024-01-15 08:51:40 +0100

    v2024-01-12

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let sudokustring = "<ul>";
 for (i = 0; i < 81; i++)
 {
-    sudokustring += "<li><input class='form-control text-center' style='font-size: 52px' value='" + i + "'></li>";
+    //sudokustring += "<li><input class='form-control text-center' style='font-size: 52px' value='" + i + "'></li>";
+    sudokustring += `<li><input class="form-control text-center" value="${i}"></li>`;
 }
 sudokustring += "</ul>";
 document.getElementById("sudoku").innerHTML = sudokustring;
